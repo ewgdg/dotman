842c36d1d63d953b49849d0bc021baf3c033ab15
Xian Zhang <xianzzz@outlook.com>
2026-09-23 19:58:08 -0700

    style(cli): color chmod actions like update

--- src/dotman/cli_style.py
+++ src/dotman/cli_style.py
@@ -28,6 +28,7 @@ TRACKED_STATE_STYLE_BY_NAME: dict[str, tuple[str, ...]] = {
 MENU_ACTION_STYLE_BY_NAME: dict[str, tuple[str, ...]] = {
     "create": ("1", "32"),
     "update": ("1", "36"),
+    "chmod": ("1", "36"),
     "delete": ("1", "31"),
     "install": ("1", "36"),
     "probe": ("1", "36"),
